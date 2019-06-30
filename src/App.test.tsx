0630e2b284a7c5e5604fdf1e0374b0b26de14bed
Justin Noel <justin.noel@calendee.com>
2019-06-30 08:33:28 -0500

    fix: use crypto library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+//@ts-ignore
+global.crypto = require('@trust/webcrypto')
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
