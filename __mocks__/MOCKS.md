c117a4b78d098d7301b789572230546ef82a375d
Justin Noel <justin.noel@calendee.com>
2019-07-05 08:31:14 -0500

    chore: update to support latest Ionic React changes

--- __mocks__/MOCKS.md
+++
@@ -1,2 +0,0 @@
-Mocks copied from: https://github.com/ionic-team/ionic/tree/master/react/__mocks__
-See also : https://github.com/ionic-team/ionic/issues/17577
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 //@ts-ignore
-global.crypto = require('@trust/webcrypto')
+global.crypto = require('@trust/webcrypto');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
